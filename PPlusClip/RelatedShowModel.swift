96e12fe0f34c31eb30aac49d58cdaeb8ddc56784
Kenny Ho <kenny.ho@cbsinteractive.com>
2021-03-24 17:20:42 -0400

    Remove tests, added shared assets

---
+++ PPlusClip/RelatedShowModel.swift
@@ -0,0 +1,8 @@
+//
+//  RelatedShowModel.swift
+//  ParamountPlusSwiftUI
+//
+//  Created by Ho, Kenny on 3/22/21.
+//
+
+import Foundation
---
+++ ParamountClip/ParamountClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParamountClipApp.swift
+//  ParamountClip
+//
+//  Created by Ho, Kenny on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct ParamountClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
